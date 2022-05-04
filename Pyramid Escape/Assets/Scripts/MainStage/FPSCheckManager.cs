c002559e6ec7d4070b6aed42f6acbdd5b556a9e5
Ws-Peroth <a01033056809@gmail.com>
2022-05-04 21:43:28 +0900

    Add Fps Check

---
+++ Pyramid Escape/Assets/Scripts/MainStage/FPSCheckManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCheckManager : MonoBehaviour
+{
+    private float _deltaTime;
+    [SerializeField] private Text fpsText;
+
+    void Update()
+    {
+        _deltaTime += (Time.unscaledDeltaTime - _deltaTime) * 0.1f;
+    }
+
+    private void OnGUI()
+    {
+        var time = _deltaTime * 1000.0f;
+        var fps = 1.0f / _deltaTime;
+        fpsText.text = $"{time:0.0} ms ({fps:0.} fps)";
+    }
+}
