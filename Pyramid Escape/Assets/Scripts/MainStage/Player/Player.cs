00f206b7ed0aaec79f88d2836bd35a713a115ccf
Ws-Peroth <a01033056809@gmail.com>
2022-06-08 10:39:49 +0900

    Add Player Get Damage

--- Pyramid Escape/Assets/Scripts/MainStage/Player/Player.cs
+++ Pyramid Escape/Assets/Scripts/MainStage/Player/Player.cs
@@ -7,12 +7,20 @@ public class Player : Entity
     public const int PlayerSortOrder = 0;
     void Start()
     {
-        
+        Hp = 50;
+        GameUIManager.instance.UpdateHpUI(Hp);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void GetDamage(float damage)
+    {
+        base.GetDamage(damage);
+        Hp -= damage;
+        GameUIManager.instance.UpdateHpUI(Hp);
     }
 }
