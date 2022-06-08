6eec69c4f76fe12fb6641664520b34873ad6423b
Ws-Peroth <a01033056809@gmail.com>
2022-06-08 10:40:02 +0900

    Fix UI Manager

--- Pyramid Escape/Assets/Scripts/Manager/GameUIManager.cs
+++ Pyramid Escape/Assets/Scripts/Manager/GameUIManager.cs
@@ -12,9 +12,14 @@ public enum SkillKnid
 
 public class GameUIManager : Singleton<GameUIManager>
 {
-    [SerializeField] private Image[] skillFillImages = new Image[3];
+    private const float HeartHp = 10;
     private readonly bool[] _isSkillDelayUIOn = new bool[3] {false, false, false};
-
+    
+    [SerializeField] private Image[] skillFillImages = new Image[3];
+    [SerializeField] private Image[] hpUI = new Image[5];
+    [SerializeField] private Text overHpText;
+    [SerializeField] private Text hpText;
+    
     public IEnumerator SkillDelay(float skillDelay,  SkillKnid kind)
     {
         var index = (int) kind;
@@ -34,5 +39,30 @@ public class GameUIManager : Singleton<GameUIManager>
         }
         _isSkillDelayUIOn[index] = false;
     }
-    
+    private void HeartActive(int count, float remain)
+    {
+        print($"count: {count}, remain: {remain}, fill: {(int) (remain / HeartHp)}");
+        for (var i = 0; i < 5; i++)
+        {
+            var fill = i < count ? 1 : 0f;
+            fill = i == count ? remain / HeartHp : fill;
+            hpUI[i].fillAmount = fill;
+            print($"obj [{i}] => {fill}");
+        }
+    }
+
+    public void UpdateHpUI(float hp)
+    {
+        hpText.text = $"Hp : {((int) hp).ToString()}";
+        var heartCount = (int) (hp / HeartHp);
+        var remainHp = hp % HeartHp;
+        HeartActive(heartCount, remainHp);
+        if (heartCount <= 5)
+        {
+            overHpText.text = "";
+            return;
+        }
+        var v = (int) (heartCount + Mathf.Ceil(remainHp));
+        overHpText.text = $"+{v.ToString()}";
+    }
 }
